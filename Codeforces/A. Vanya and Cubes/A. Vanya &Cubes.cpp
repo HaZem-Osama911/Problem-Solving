c5232698f9b47409eeb30eb589996e9e1f7df3f2
Ha-Zem <118283249+HaZem-Osama911@users.noreply.github.com>
2024-11-30 19:00:59 +0200

    Create A. Vanya &Cubes.cpp

---
+++ Codeforces/A. Vanya and Cubes/A. Vanya &Cubes.cpp
@@ -0,0 +1,89 @@
+/*
+	--------------------------------------( Problem Link )-----------------------------------------------
+
+
+
+
+
+	-----------------------------------------------------------------------------------------------------
+
+
+							  ██╗░░██╗░█████╗░███████╗███████╗███╗░░░███╗
+							  ██║░░██║██╔══██╗╚════██║██╔════╝████╗░████║
+							  ███████║███████║░░███╔═╝█████╗░░██╔████╔██║
+							  ██╔══██║██╔══██║██╔══╝░░██╔══╝░░██║╚██╔╝██║
+							  ██║░░██║██║░░██║███████╗███████╗██║░╚═╝░██║
+							  ╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░░░░╚═╝
+
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+#define ll						     long long
+#define E						     '\n'
+#define sp						     " "
+#define C						     cout
+#define Ci						     cin
+#define br						     cout<<endl
+#define pb							 push_back
+#define asort(a,n)				     sort(a,a+n)
+#define PYES					     cout<<"YES"<<E;
+#define PNO						     cout<<"NO"<<E;
+#define PYes					     cout<<"Yes"
+#define PNo					   	     cout<<"No"
+#define Pyes					     cout<<"yes"
+#define Pno						     cout<<"no"
+#define							     f1 cout<<"----- 1 -----"<<endl;
+#define							     f2 cout<<"----- 2 -----"<<endl
+#define Fp(e)					     for(int i=0;i<e;i++)
+#define Fps(s,e)				     for(int i=s;i<e;i++)
+#define Fpj(e)					     for(int j=0;j<e;j++)
+#define Fm(e)						 for(int i=0;i<f;i--)
+#define Fac(e)						 Fp(e){ res+=res*i;}cout<<res;
+#define Rev(s,len)					 Fm(len) {ans+=s[i];}cout<<ans;
+#define HaZem 						 ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define arr(n)						 int* arr=new int[n];	 
+
+
+//--------------------------------------( Const )-----------------------------------------------
+
+
+//--------------------------------------( function )-----------------------------------------------
+
+void solve();
+
+int main()
+{
+	HaZem;
+
+	solve();
+
+	return 0;
+}
+
+void solve()
+{
+	int n, m = 1, i = 1, step = 0;
+	cin >> n;
+	
+	while (m <= n)
+	{
+		m = ((i * i) + i) / 2;
+
+		if (m > n)break;
+
+		n -= m;
+
+		i++;
+		step++;
+	}
+	cout << step << E;
+
+}
