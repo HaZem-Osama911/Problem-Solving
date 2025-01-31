1db7587f9ba1bd8654dec0f2a3b971b713753f6d
Ha-Zem <118283249+HaZem-Osama911@users.noreply.github.com>
2025-01-31 19:20:37 +0200

    Create B. Count characters.cpp

---
+++ Codeforces/B. Count characters/B. Count characters.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+
+
+
+using namespace std;
+
+#define ll						     long long
+#define E						     '\n'
+#define sp						     " "
+#define C						     cout
+#define Ci						     cin
+#define br						     cout<<endl
+#define pb							 push_back
+#define asort(a,n)				     sort(a,a+n)
+#define PYES					     cout<<"YES"<<E;
+#define PNO						     cout<<"NO"<<E;
+#define PYes					     cout<<"Yes"
+#define PNo					   	     cout<<"No"
+#define Pyes					     cout<<"yes"
+#define Pno						     cout<<"no"
+#define br						     cout<<string(50,'-')<<endl;		
+#define							     f1 cout<<"----- 1 -----"<<endl;
+#define							     f2 cout<<"----- 2 -----"<<endl
+#define Fp(e)					     for(int i=0;i<e;i++)
+#define Fps(s,e)				     for(int i=s;i<e;i++)
+#define Fpj(e)					     for(int j=0;j<e;j++)
+#define Fm(e)						 for(int i=0;i<f;i--)
+#define Fac(e)						 Fp(e){ res+=res*i;}cout<<res;
+#define Rev(s,len)					 Fm(len) {ans+=s[i];}cout<<ans;
+#define HaZem 						 ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define arr(n)						 int* arr=new int[n];	 
+//--------------------------------------( Const )-----------------------------------------------
+
+//--------------------------------------( function )-----------------------------------------------
+
+void solve();
+
+int main()
+{
+	HaZem
+	
+
+	solve();
+
+	return 0;
+}
+
+void solve()
+{
+
+	string s; cin >> s;
+
+	map <char, int>ch;
+	for (int i = 0; i < s.length(); i++)
+	{
+		ch[s[i]]++;
+	}
+	for (auto& c : ch)
+	{
+		cout << c.first << sp << c.second << endl;;
+	}
+}
