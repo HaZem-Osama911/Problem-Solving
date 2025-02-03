28d6314dc53432e814e8760fdb5a6139e27931a3
Ha-Zem <118283249+HaZem-Osama911@users.noreply.github.com>
2025-02-03 04:46:38 +0200

    Create code.cpp

---
+++ Assiut Training/E. Range sum query/code.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+#define ll						     long long
+#define E						     '\n'
+#define sp						     " "
+#define C						     cout
+#define Ci						     cin
+#define br						     cout<<endl
+#define pb							 push_back
+#define asort(a,n)				     sort(a,a+n)
+#define PYES					     cout<<"YES"<<E;
+#define PNO						     cout<<"NO"<<E;
+#define PYes					     cout<<"Yes"
+#define PNo					   	     cout<<"No"
+#define Pyes					     cout<<"yes"
+#define Pno						     cout<<"no"
+#define br						     cout<<string(50,'-')<<endl;		
+#define							     f1 cout<<"----- 1 -----"<<endl;
+#define							     f2 cout<<"----- 2 -----"<<endl
+#define Fp(e)					     for(int i=0;i<e;i++)
+#define Fps(s,e)				     for(int i=s;i<e;i++)
+#define Fpj(e)					     for(int j=0;j<e;j++)
+#define Fm(e)						 for(int i=0;i<f;i--)
+#define Fac(e)						 Fp(e){ res+=res*i;}cout<<res;
+#define Rev(s,len)					 Fm(len) {ans+=s[i];}cout<<ans;
+#define HaZem 						 ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define arr(n)						 int* arr=new int[n];	 
+//--------------------------------------( Const )-----------------------------------------------
+
+
+
+//--------------------------------------( function )-----------------------------------------------
+
+void solve();
+void pre(ll arr[], int size, ll arrp[]);
+// int binarySearch(ll arr[], int low, int high, int val);
+int main()
+{
+	HaZem;
+	
+
+	solve();
+   
+	return 0;
+}
+
+void solve()
+{
+	int len, l, r, q; cin >> len >> q;
+	ll* arr = new ll[len];
+	ll* arrp = new ll[len];
+
+	Fp(len)cin >> arr[i];
+
+	pre(arr, len, arrp);
+	
+	while (q--)
+	{
+		cin >> l >> r;
+		if (l == 1)C << arrp[r - 1] << E;
+		else
+		{
+			C << arrp[r - 1] - arrp[l - 2] << E;
+		}
+	}
+	
+}
+void pre(ll arr[], int size, ll arrp[])
+{
+	arrp[0] = arr[0];
+	for (int i = 1; i < size; i++)
+	{
+		arrp[i] = arrp[i - 1] + arr[i];
+	}
+}
