d7fe5e4ffce2179e7d589f3f4f0e7145a62df2bc
Ha-Zem <118283249+HaZem-Osama911@users.noreply.github.com>
2025-02-06 19:32:59 +0200

    Create code.cpp

---
+++ Assiut Training/A. set/code.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+
+using namespace std;
+
+#define ll						     long long
+#define E						     '\n'
+#define sp						     " "
+#define C						     cout
+#define Ci						     cin
+#define br						     cout<<endl
+#define pb							 push_back
+#define asort(a,n)				     sort(a,a+n)
+#define PYES					     cout<<"YES"<<E;
+#define PNO						     cout<<"NO"<<E;
+#define PYes					     cout<<"Yes"
+#define PNo					   	     cout<<"No"
+#define Pyes					     cout<<"yes"
+#define Pno						     cout<<"no"
+#define br						     cout<<string(50,'-')<<endl;		
+#define							     f1 cout<<"----- 1 -----"<<endl;
+#define							     f2 cout<<"----- 2 -----"<<endl
+#define Fp(e)					     for(int i=0;i<e;i++)
+#define Fps(s,e)				     for(int i=s;i<e;i++)
+#define Fpj(e)					     for(int j=0;j<e;j++)
+#define Fm(e)						 for(int i=0;i<f;i--)
+#define Fac(e)						 Fp(e){ res+=res*i;}cout<<res;
+#define Rev(s,len)					 Fm(len) {ans+=s[i];}cout<<ans;
+#define HaZem 						 ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define arr(n)						 int* arr=new int[n];	
+
+//--------------------------------------( Const )-----------------------------------------------
+
+
+
+//--------------------------------------( function )-----------------------------------------------
+
+void solve();
+
+// int binarySearch(ll arr[], int low, int high, int val);
+int main()
+{
+	HaZem;
+	
+
+	solve();
+   
+	return 0;
+}
+
+void solve()
+{
+	ll q, val; cin >> q;
+	string s;
+	set<ll> se;
+	while (q--)
+	{
+		cin >> s >> val;
+		if (s == "insert")
+		{
+			se.insert(val);
+		}
+		else if (s == "upper_bound")
+		{
+			auto it = se.upper_bound(val);
+			(it != se.end()) ? C << *it << endl : C << "-1" << E;
+		}
+		else if (s == "lower_bound")
+		{
+			auto it = se.lower_bound(val);
+			(it != se.end()) ? C << *it << endl : C << "-1" << E;
+		}
+		else
+		{
+			auto it = se.find(val);
+			if (it != se.end())C << "found" << E;
+			else C << "not found" << E;
+		}
+	}
+}
+ 
